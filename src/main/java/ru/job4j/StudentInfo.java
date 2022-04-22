068675e3ea64666e91367c260983c286ed3e28bc
opossum <ryabkoff@rambler.ru>
2022-04-22 13:19:47 +0500

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey Ryabkov");
+        System.out.println("21.08.1981");
+    }
+}
