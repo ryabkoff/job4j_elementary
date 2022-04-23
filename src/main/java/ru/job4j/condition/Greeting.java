718870896c4964b7478a341347cbc74d4e5022ca
opossum <ryabkoff@rambler.ru>
2022-04-23 18:56:42 +0500

    .

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -8,5 +8,27 @@ public class Greeting {
         int year = 2022;
         idea += year;
         System.out.println(idea);
+
+//        long l = 129;
+//        byte b = (byte) l;
+//        System.out.println(b);
+
+//        float f = 12358.7f;
+//        int i = 45981;
+//        short rsl = (short) (f + i);
+//        System.out.println(rsl);
+
+//        char c = 45000;
+//        float i = c;
+//        System.out.println(i);
+
+//        double d = 121.19;
+//        byte b = (byte) d;
+//        System.out.println(b);
+
+        short s = 1500;
+        char c = (char) s;
+        double d = c;
+        System.out.println(d);
     }
 }
