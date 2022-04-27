647f2a8561aa402ff76c39097069efb878ff481f
opossum <ryabkoff@rambler.ru>
2022-04-27 17:26:12 +0500

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenSumEvenNumbersFromOneToThirtyThen240() {
+        int start = 1;
+        int finish = 30;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 240;
+        Assert.assertEquals(expected, result);
+    }
+}
