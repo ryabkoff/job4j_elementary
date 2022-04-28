88c4d8f2cb8e5a57c82f041f0c4ca23608b95376
opossum <ryabkoff@rambler.ru>
2022-04-28 09:38:52 +0500

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Sergey Ryabkov";
+        names[2] = "Elena Kartashova";
+        names[3] = "Stas Korobeinikov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
